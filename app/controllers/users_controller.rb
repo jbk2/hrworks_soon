593c59c76077c966e4d6e2c1fa4212e00e628246
James B Kemp <jamesbkemp@me.com>
2015-05-16 23:02:21 +0100

    getting model working, routes renderring, styling done, tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
 
   def index
-    
+    @users = User.all
+    @user = User.new #This is strange, an issue to do with routing when creating a
+    # new object means @user, singular, needs to be instantiated in user#index
   end
 
 
   def new
-
+    @user = User.new
   end
-  
+
+
+  def create
+    @user = User.create(params[:user].permit(:name, :email))
+
+    if @user.save
+      redirect_to users_path, :notice => 'You have been registered'
+    else
+      render 'show'
+    end
+  end
+
 end
--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -4,7 +4,7 @@ describe 'users index page' do
   context 'no users signed in' do
     it 'should display a message' do
       visit '/users'
-      expect(page).to have_content 'not signed in'
+      expect(page).to have_content 'Sign up'
     end
   end
 end
@@ -13,10 +13,10 @@ describe 'registering a user' do
   it 'confirms user registration' do
     visit '/users/new'
     fill_in 'Name here...', with: 'test name'
-    fill_in 'Your email here...', with: 'test1@test.com'
+    fill_in 'Your email here...', with: 'test2@test.com'
     click_button 'Notify me'
     expect(current_path).to eq '/users'
-    expect(page).to have_content "test1@test.com"
-    expect(page).not_to have_content "not signed in"
+    expect(page).to have_content "test2@test.com"
+    expect(page).not_to have_content "Sign up"
   end
 end
