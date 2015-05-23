097e96dcc46a7ec5de90423076a63d53ee7d814f
James B Kemp <jamesbkemp@me.com>
2015-05-23 23:34:58 +0100

    adding failing first name test

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -13,11 +13,11 @@ end
 describe 'registering a user' do
   it 'confirms user registration' do
     visit '/users/new'
-    fill_in 'Name here...', with: 'test name'
+    fill_in 'Name here...', with: 'james kemp'
     fill_in 'Your email here...', with: 'test2@test.com'
     click_button 'Notify me'
     # expect(current_path).to eq '/users/@user.id'
-    expect(page).to have_content "Welcome test2@test.com"
+    expect(page).to have_content "Welcome James"
     expect(page).not_to have_content "Sign up"
   end
 end
