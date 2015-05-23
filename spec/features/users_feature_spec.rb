19394e23613062ddea06970677720301497985a8
James B Kemp <jamesbkemp@me.com>
2015-05-23 19:04:36 +0100

    tidy tests, css and show page

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 describe 'users index page' do
   context 'no users signed in' do
     it 'should display a message' do
@@ -16,7 +17,8 @@ describe 'registering a user' do
     fill_in 'Your email here...', with: 'test2@test.com'
     click_button 'Notify me'
     # expect(current_path).to eq '/users/@user.id'
-    expect(page).to have_content "test2@test.com"
+    expect(page).to have_content "Welcome test2@test.com"
     expect(page).not_to have_content "Sign up"
   end
 end
+
