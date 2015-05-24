7744027df3f09358354daa7a3cb9416fda0ebc48
James B Kemp <jamesbkemp@me.com>
2015-05-25 00:22:22 +0100

    adding a greeting helper in view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def greet(hour_of_clock)
+    if hour_of_clock >= 1 && hour_of_clock <= 11
+      "Morning"
+    elsif hour_of_clock >= 12 && hour_of_clock <= 16
+      "Afternoon"
+    else
+      "Evening"
+    end
+  end
+
 end
