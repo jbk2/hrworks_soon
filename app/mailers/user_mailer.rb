e012fec017cd84afb5318d4945a24a4bf68b3092
James B Kemp <jamesbkemp@me.com>
2015-05-27 01:38:39 +0100

    completing development mailer process

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  default from: "hello@hrworks.co"
+
+  helper :users
+
+  def welcome_email(user)
+    @user = user
+    # @url = 
+    mail(to: @user.email, subject: 'Welcome to hrworks.io')
+  end
+
+end
