5e7dbeb3934e91f6d2141a71dc5af68a4f9b2cb9
James B Kemp <jamesbkemp@me.com>
2015-06-03 10:42:18 +0100

    adding new mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     # @url = 
-    mail(to: @user.email, subject: 'Welcome to hrworks.io')
+    mail(to: @user.email, subject: 'Welcome to Hrworks')
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.asset_host = 'http://hrworks.co'
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
