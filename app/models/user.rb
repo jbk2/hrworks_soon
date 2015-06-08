2f4c8b54e6505a9a9c4ec6a7f4aa46e7f4470b95
James B Kemp <jamesbkemp@me.com>
2015-06-08 16:44:15 +0100

    corrected spelling mistake in user model email uniqueness validation error mssg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { minimum: 3 , message: "is too short (≥3 chars)" }#,message: 'custom message here'
-  validates :email, presence: true, uniqueness: {message: "is already registerred", case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :email, presence: true, uniqueness: {message: "is already registered", case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   after_create :send_welcome_email
   
 
