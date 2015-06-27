6d8aeafc3b0af072040ee2f5d8098ec8fb420cd6
James B Kemp <jamesbkemp@me.com>
2015-06-27 23:57:34 +0100

    mailchimp ActiveJob adding users to mailing list fine

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -1,8 +1,8 @@
 class SubscribeUserToMailingListJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
+  def perform(user)
     gb = Gibbon::API.new
-    gb.lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email = {:email => user.email }, :double_optin => false})
+    gb.lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => user.email }, :double_optin => false})
   end
 end
