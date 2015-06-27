80ab6656066cc6806ab20bb6f3b274831722e024
James B Kemp <jamesbkemp@me.com>
2015-06-27 23:46:07 +0100

    adding dot env gem, putting mailchimp keys in dot env, generating ActiveJob and writing code for SubscribeUserToMailingListJob

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'bigdecimal', '~> 1.2'
 
 gem 'rails_12factor', group: :production
 
+gem 'dotenv-rails', :groups => [:development]
+
+gem 'gibbon'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,18 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { minimum: 3 , message: "is too short (≥3 chars)" }#,message: 'custom message here'
   validates :email, presence: true, uniqueness: {message: "is already registered", case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
+  after_create :subscribe_user_to_mailing_list
   
 
   def send_welcome_email
     UserMailer.welcome_email(self).deliver_now
+  end
+
+  private
+
+  def subscribe_user_to_mailing_list
+    SubscribeUserToMailingListJob.perform_later(self)
   end
 
 end
